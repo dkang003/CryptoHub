7d170a3dd09868576cf890f59d317136fe3662f5
Daniel Kang <dkang@gennet.com>
2022-02-04 20:10:19 -0800

    Before push to github

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+
+import { useGetCryptosQuery  } from '../services/cryptoApi';
 
 export const Cryrptocurrencies = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+  const [cryptos, setCryptos] = useState(data?.data?.coins);
+
+  console.log(cryptos);
+
   return (
-    <div>
-        Cryrptocurrencies
-    </div>
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+                <Card
+                  title={`${currency.rank}. ${currency.name}`}
+                  extra={<img className="crypto-image" src={currency.iconUrl} />}
+                  hoverable
+                >
+                  
+                </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   );
 };
 
